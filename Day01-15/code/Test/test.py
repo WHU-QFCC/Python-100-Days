6b17bfab7498622ad556b7ae84625517a42c732a
WHU-QFCC <1129162053@qq.com>
2020-11-02 19:11:42 +0800

    添加Test文件夹

---
+++ Day01-15/code/Test/test.py
@@ -0,0 +1,43 @@
+# s1 = 'hello, world!'
+# s2 = "hello, world!"
+# # 以三个双引号或单引号开头的字符串可以折行
+# # 加入\可以让字符串不转行
+# s3 = """
+# hello, \
+# world!
+# """
+# print(s1, s2, s3, end='')
+
+# s1 = '\'hello, world!\''
+# s2 = '\n\\hello, world!\\\n'
+# print(s1, s2, end='')
+
+# s1 = '\141\142\143\x61\x62\x63'
+# s2 = '\u9a86\u660a'
+# print(s1, s2)
+
+# s1 = r'\'hello, world!\''
+# s2 = r'\n\\hello, world!\\\n'
+# print(s1, s2, end='')
+
+# print()
+# str1 = 'psznb'
+# print(str1.center(50, '&'))
+
+# print("bbb", "jjjj")
+
+# print([2]*3)
+
+# names = ['关羽', '张飞', '赵云', '马超', '黄忠']
+# print(enumerate(names))
+# for row, name in enumerate(names):
+#     print(row, name)
+
+# str = 'qwertyuiopqwertyuiopqwer'
+# print(str.find('er'), str.rfind('er'))
+
+str = '1234'
+print(str[::-1])
+
+# yh = [[]] * 4
+# print(yh)
